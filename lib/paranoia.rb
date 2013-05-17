b7be747c7bacbbe984d2ecf3a1f0385b1ecdee5d
delba <delba@server.fake>
2013-05-18 00:06:35 +0200

    use update_column

--- lib/paranoia.rb
+++ lib/paranoia.rb
@@ -20,24 +20,17 @@ module Paranoia
   end
 
   def delete
-    update_attribute_or_column(:deleted_at, Time.now) if !deleted? && persisted?
+    update_column(:deleted_at, Time.now) if !deleted? && persisted?
   end
 
   def restore!
-    update_attribute_or_column :deleted_at, nil
+    update_column :deleted_at, nil
   end
 
   def destroyed?
     !self.deleted_at.nil?
   end
   alias :deleted? :destroyed?
-
-  private
-
-  # Rails 3.1 adds update_column. Rails > 3.2.6 deprecates update_attribute, gone in Rails 4.
-  def update_attribute_or_column(*args)
-    respond_to?(:update_column) ? update_column(*args) : update_attribute(*args)
-  end
 end
 
 class ActiveRecord::Base
